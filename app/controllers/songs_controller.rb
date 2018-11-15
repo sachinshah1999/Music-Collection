82131bd5fdda750e384e10e98da26bbcfc17408d
Sachin P. Shah <ss01703@surrey.ac.uk>
2018-11-15 23:27:44 +0000

    Added Song Show View

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,14 @@
 class SongsController < ApplicationController
+  # filter method to find the song before the particular controller actions
+  before_action :find_song, only: [:show, :edit, :update, :destroy]
+
   # define index action to display all songs
   def index
+    @songs = Song.all.order("created_at DESC")
+  end
+
+  # define show action to display a specific song
+  def show
   end
 
   # define new action for returning an simple form for creating the song
@@ -24,6 +32,11 @@ end
   # method allows us to chose which attributes should be whitelisted for mass updating.
   def song_params
     params.require(:song).permit(:title, :artist, :album, :genre, :year)
+  end
+
+  # method to find a specific song using id
+  def find_song
+    @song = Song.find(params[:id])
   end
 
 end
