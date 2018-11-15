488e3d23450dac8e2ad28164faf160f7d80b977e
Sachin P. Shah <ss01703@surrey.ac.uk>
2018-11-15 22:55:17 +0000

    Added Song New View

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,23 @@
 class SongsController < ApplicationController
-  # create index action
+  # define index action to display all songs
   def index
   end
-  
+
+  # define new action for returning an simple form for creating the song
+  def new
+    @song = Song.new
+  end
+
+  # define create action for creating songs
+  def create
+    @song = Song.new(song_params)
+  end
+
+  private
+
+  # method allows us to chose which attributes should be whitelisted for mass updating.  
+  def song_params
+    params.require(:song).permit(:title, :artist, :album, :genre, :year)
+  end
+
 end
