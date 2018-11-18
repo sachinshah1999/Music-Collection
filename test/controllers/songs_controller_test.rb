6277fff23433f47f149ec2099646cb3567581ed8
Sachin P. Shah <ss01703@surrey.ac.uk>
2018-11-18 16:35:48 +0000

    Added Basic Songs Controller Testing

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -4,8 +4,18 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @song = songs(:one)
+  end
+
   test "should get index" do
     get songs_url
+    assert_response :success
+  end
+
+  test "should get index again" do
+    get '/songs'
     assert_response :success
   end
 end
