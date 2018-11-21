edc6efe7193a205f63990de6596c5d980617d504
Sachin P. Shah <ss01703@surrey.ac.uk>
2018-11-21 17:58:16 +0000

    Added Genre Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181118195126) do
+ActiveRecord::Schema.define(version: 20181121175635) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
